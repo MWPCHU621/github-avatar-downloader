8c81b6b5b20bbdac709cd9ed51cf8623e2119bf3
Patrick Chu <mingweipatrickc@gmail.com>
2018-09-03 20:10:47 +0000

    created downloadImageByURL function to download a given url image and save/create to prenamed filepath.

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,11 @@
 var request = require('request');
 var token = require('./secrets.js');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+//gets all of the contributors of a given repo and repo owner
+//and returns an array of objects containing information about the contributors.
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -19,10 +22,36 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 }
 
+//
+function downloadImageByURL(url, filePath) {
+  request(url)
+
+    .on('error', function(err) {
+      throw err;
+    })
+
+    .on('response', function(response) {
+    console.log("downloading images....");
+    console.log(response.statusCode, response.statusMessage, response.headers['content-type'])
+
+    })
+
+    .on('end', function(respose) {
+      console.log("download complete");
+    })
+
+    .pipe(fs.createWriteStream(filePath));
+}
+
+
+//downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+
+
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   for(let i = 0; i < result.length; i++)
   {
-    console.log(result[i]["avatar_url"]);
+    let filePath = "avatars/" + result[i]["login"];
+    downloadImageByURL(result[i]["avatar_url"], filePath);
   }
 });
