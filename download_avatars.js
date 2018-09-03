02dacc0f715b07a4d6f00a17b35c99e5782b02e7
Patrick Chu <mingweipatrickc@gmail.com>
2018-09-03 20:30:15 +0000

    added comments where I deemed needed

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,4 @@
+//required files and methods/api.
 var request = require('request');
 var token = require('./secrets.js');
 var fs = require('fs');
@@ -14,6 +15,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
+  //converts the requests body and converts to an actual javascript object/array.
   request(options, function(err, res, body) {
     let requested = JSON.parse(body);
     cb(err, requested);
@@ -21,40 +23,53 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 }
 
-//
+//download the images provided by url, and saves the image to filePath
+//or create the file at filePath destination if it does not exist.
 function downloadImageByURL(url, filePath) {
   request(url)
 
+    //if url is invalid, throws the error.
     .on('error', function(err) {
       throw err;
     })
 
+    //if response is valid, console logs download status.
     .on('response', function(response) {
     console.log("downloading images....");
     console.log(response.statusCode, response.statusMessage, response.headers['content-type'])
 
     })
 
+    //on finish, console logs completed message.
     .on('end', function(respose) {
       console.log("download complete");
     })
 
+    //pipes the result to filePath and saves it if it already exists
+    // or creates the file if it doesn't exist.
     .pipe(fs.createWriteStream(filePath));
 }
 
 
-//downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+//-------------MAIN CODE TO RUN DOWNLOAD_AVATARS.JS FILE---------------
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+//gets the bash arguments and removes the first 2 items in the array.
 let myArgs = process.argv.slice(2);
 
+
+//checks to see if the argument array length is exactly 2.
+//if not, prints error message and does not continue.
 if(myArgs.length !== 2) {
   console.log("ERROR. did not provide correct argument count.");
 }
 else {
+  //function on line 9.
   getRepoContributors(myArgs[0], myArgs[1], function(err, result) {
     console.log("Errors:", err);
+
     for(let i = 0; i < result.length; i++)
     {
       let filePath = "avatars/" + result[i]["login"];
